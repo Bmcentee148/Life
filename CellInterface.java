eb5f331e9afbc04deda89d751ab3413a00252533
Brian McEntee <brian.m.mcentee@gmail.com>
2014-07-11 00:40:50 -0400

    completed CellTable's interface

--- CellInterface.java
+++ CellInterface.java
@@ -1,26 +1,33 @@
-/** *********************************************************
+/** ************************************************************
 	<p>This is the interface for the class {@code Cell} and will
 	define its basic functions and procedures. </p>
 
 	@author Brian McEntee
-	@version 7.10.14
-*********************************************************** */
+	@version 7.10.14 12.21am
+*****************************************************************/
 
 public interface CellInterface{
 
-	/**
-	<p>This procedure will set the status of the cell to 
-	on or off depending on the given parameter.</p>
+	/** <p> This procedure will set the status of the cell to 
+		on or off.</p>
 
 	@param newStatus If false the Cell will be set to 'off',
-		if true the Cell will be set to 'on'.
-	*/
+		if true the Cell will be set to 'on'. */
 	public void setStatus(boolean newStatus);
 
-	/**
-	
-	*/
+	/** <p>This function will return the status of the {@code Cell}
+		object .</p>
 
+	@return status The current status of the object. True is 
+		defined as 'on' and false 'off.  */
+	public boolean getStatus();
 
+	/** <p> This procedure will update the status of the 
+		{@code Cell} object based on the number of neighbors it 
+		has that are turned 'on'.</p>
+
+		@param numNeighborsOn the number of neighbors a {@code Cell} 
+		object has that are turned 'on'. */
+	public void updateStatus(int numNeighborsOn);
 
 }
---
+++ CellTableInterface.java
@@ -0,0 +1,29 @@
+/** ************************************************************
+<p> This is the interface for {@code CellTable} and will define 
+	its basic functions and procedures. It will be comprised of
+	and depend on being formulated by {@code Cell} objects. </p>
+
+	@author Brian McEntee
+	@version 7.11.2014 12.21am
+
+***************************************************************/
+
+public interface CellTableInterface{
+
+	/** <p> This procedure will initialize the CellTable. This
+		method MUST be done before any new generations of the 
+		table can be created. </p> */
+
+
+	/** <p> This procedure will bring about a new generation of the 
+		game. The {@code cellGrid} will be changed based on the 
+		number of neighbors that were turned 'on' for each 
+		{@code Cell} in the {@code CellTable}.</p> */
+	public void newGeneration();
+
+	/** <p> This procedure will display the {@code CellTable}'s 
+		current state. </p> */
+	public void display();
+
+
+}
